124d4768246db05062f955f7320d5057d11ea4e3
Sanjoy1210 <62760864+Sanjoy1210@users.noreply.github.com>
2020-10-07 20:37:42 +0600

    lab

---
+++ Employee.java
@@ -0,0 +1,17 @@
+package javalab;
+public class Employee {
+	private String name;
+	private double salary;
+	public Employee(String name, double salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+}
---
+++ Organization.java
@@ -0,0 +1,11 @@
+package javalab;
+
+public class Organization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee employee = new Employee("Hablu", 40000.500);
+		System.out.println(employee.getName() + " " + employee.getSalary());
+	}
+
+}
